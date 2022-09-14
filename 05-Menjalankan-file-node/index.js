4b4808d214f680e22ad0bbe4f696b365ca476129
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-09-14 09:11:26 +0700

    menjalankan file node

---
+++ 05-Menjalankan-file-node/index.js
@@ -0,0 +1,7 @@
+// const nama = 'Adiansyah Putra';
+// const cetakNama = (nama) => `Hi, nama saya ${nama}`;
+// console.log(cetakNama(nama));
+const cetakNama = require('./coba');
+
+// console.log('hello wpu!');
+console.log(cetakNama('adi'));
