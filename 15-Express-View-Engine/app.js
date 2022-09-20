4c50760d5ba0fd8d5884c059eaa9972c83735a00
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-09-20 11:08:20 +0700

    Express View Engine

---
+++ 15-Express-View-Engine/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const port = 3000;
+
+// gunakan ejs
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+app.get('/', (req, res) => {
+  // res.sendFile('./index.html', { root: __dirname });
+  const mahasiswa = [
+    {
+      nama: 'Adiansyah Putra',
+      email: 'adiansyahh424@gmail.com',
+    },
+    {
+      nama: 'Erik',
+      email: 'erik@gmail.com',
+    },
+    {
+      nama: 'Doddy',
+      email: 'dody@gmail.com',
+    },
+  ];
+  res.render('index', {
+    nama: 'Adiansyah Putraa',
+    title: 'Halaman Home',
+    layout: 'layouts/main-layout',
+    mahasiswa,
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { layout: 'layouts/main-layout', title: 'Halaman About' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { layout: 'layouts/main-layout', title: 'Halaman Contact ehe' });
+});
+
+app.get('/product/:id', (req, res) => {
+  res.send(`Product ID : ${req.params.id} <br> Category ID : ${req.query.category}`);
+});
+
+app.use('/', (req, res) => {
+  res.status(404);
+  res.send('<h1>404</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
